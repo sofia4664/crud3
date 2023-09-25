5b8e5b32639d9e20d7946215632268bc9495e85d
sofia4664 <105325880+sofia4664@users.noreply.github.com>
2023-09-24 21:43:15 -0600

    Update EditBlog.js

--- reactfront/src/blog/EditBlog.js
+++ reactfront/src/blog/EditBlog.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-const URI = 'http://localhost:8000/blogs/'
+const URI = 'http://54.205.117.250:8000/blogs/'
 
 const CompEditBlog = () => {
     const [title, setTitle] = useState('');
